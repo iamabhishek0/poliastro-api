f27f282d2f9028589a582d84b50c06d551ad04dd
Abhishek Chaurasia <abhi24061997@gmail.com>
2020-03-24 03:20:44 +0530

    orbit_example REST API

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, jsonify, request
+import os
+import poliastro
+from astropy.units.core import PrefixUnit
+import importlib
+from poliastro.twobody import Orbit
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+app.debug = True
+
+@app.route('/')
+def hello_world():
+    target = os.environ.get('TARGET', 'World')
+    return 'Hello {}!\n'.format(target)
+
+@app.route('/orbit', methods=['GET', 'POST'])
+def orbit_example():
+    if request.is_json:
+
+        # load JSON data
+        req = request.get_json()
+
+        # getting the body from JSON and importing it from poliastro
+        body = req.get("body")
+        import_path = "poliastro.bodies"
+        bodies = importlib.import_module(import_path)
+        body = getattr(bodies, body)
+
+        # loading position magnitude and unit and unifying it
+        position_value = req.get("position").get("value")
+        position_unit = req.get("position").get("unit")
+        position_unit = PrefixUnit(position_unit)
+        r = position_value * position_unit
+
+        # loading velocity magnitude and unit and unifying it
+        velocity_value = req.get("velocity").get("value")
+        velocity_unit = req.get("velocity").get("unit")
+        velocity_unit = velocity_unit.split("/")
+        velocity_unit = list(map(PrefixUnit, velocity_unit))
+        v = velocity_value * velocity_unit[0] / velocity_unit[1]
+
+        # Calculating Orbit
+        final_orbit = Orbit.from_vectors(body, r, v)
+
+        pericenter_radius = final_orbit.r_p.value
+        apocenter_radius = final_orbit.r_a.value
+        inclination = final_orbit.inc.value
+        reference_frame = str(final_orbit.frame)
+        attractor = str(final_orbit.attractor)
+        epoch = final_orbit.epoch.value
+
+        return jsonify(pericenter_radius=pericenter_radius, apocenter_radius=apocenter_radius, inclination=inclination, reference_frame=reference_frame, attractor=attractor, epoch=epoch)
+
+
+if (__name__ == '__main__'):
+    app.run(debug=True, host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
